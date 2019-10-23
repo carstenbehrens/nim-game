133996e070c709ccb7af31de29aabf892d879c0e
carstenbehrens <carsten.behrens1@googlemail.com>
2019-10-23 22:46:32 +0200

    Add more tests for Match component

--- src/components/Match/index.js
+++ src/components/Match/index.js
@@ -1,52 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const MatchContainer = styled.div`
-	height: 8rem;
-	width: 4rem;
-	cursor: pointer;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(255,255,255,0.1) 100%);
-  border-radius: 3rem;
-
-  @media (min-width: 900px) {
-    &:hover {
-      box-shadow: 0px 3px 46px -17px rgba(0,0,0,0.3);
-      background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(255,255,255,0.3) 100%);
-    }
-  }
-  
-  @media (max-width: 600px) {
-    height: 90%;
-  }
-`;
-
-const MatchStick = styled.div`
-	height: 5rem;
-	width: 0.6rem;
-	background: rgb(224,175,76);
-  background: linear-gradient(270deg, rgba(224,175,76,1) 0%, rgba(224,175,76,1) 50%, rgba(230,186,102,1) 50%, rgba(237,201,136,1) 100%);
-  cursor: pointer;
-  opacity: ${props => props.visible ? '100' : '0'};
-`;
-
-const MatchTip = styled.div`
-  height: 1.4rem;
-  width: 1rem;
-  border-radius: 40%;
-  background: rgb(89,21,12);
-  background: linear-gradient(126deg, rgba(89,21,12,1) 0%, rgba(238,141,101,1) 0%, rgba(193,104,66,1) 37%, rgba(213,49,28,1) 39%, rgba(71,19,12,1) 100%);
-  opacity: ${props => props.visible ? '100' : '0'};
-`
+import { MatchContainer, MatchTip, MatchStick } from './style';
 
 export default ({ visible, number, onClick }) => {
 	return (
 		<MatchContainer onClick={() => onClick(number)}>
-      <MatchTip visible={visible} />
-			<MatchStick visible={visible} /> 
-    </MatchContainer>
+			<MatchTip visible={visible} />
+			<MatchStick visible={visible} />
+		</MatchContainer>
 	);
 };
--- src/components/Match/index.test.js
+++ src/components/Match/index.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import toJson from 'enzyme-to-json'
+import toJson from 'enzyme-to-json';
 import Match from './index';
+import { MatchContainer, MatchStick } from './style';
 
 describe('Match', () => {
-  test('renders without crashing', () => {
+	test('renders without crashing', () => {
 		shallow(<Match />);
 	});
 
@@ -13,7 +14,25 @@ describe('Match', () => {
 			<Match visible={true} key={0} matchNumber={1} onClick={() => null} />
 		);
 		expect(toJson(wrapper)).toMatchSnapshot();
-  });
-  
+	});
 
+	test('onClick gets called with the correct number', () => {
+		MatchContainer.displayName = 'MatchContainer';
+
+		const handleClickMatch = jest.fn();
+
+		const wrapper = shallow(<Match number={1} onClick={handleClickMatch} />);
+		wrapper.find('MatchContainer').simulate('click');
+
+		expect(handleClickMatch).toHaveBeenCalledWith(1);
+	});
+
+	test('visibility gets passed down', () => {
+    MatchContainer.displayName = 'MatchContainer';
+		MatchStick.displayName = 'MatchStick';
+
+		const wrapper = shallow(<Match visible={false} />);
+
+    expect(wrapper.find('MatchStick').props()).toEqual({ visible: false });
+	});
 });
