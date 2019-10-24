5ce68fda3f9dcd7243be1a5ad2707d8f67885f72
carstenbehrens <carsten.behrens1@googlemail.com>
2019-10-24 22:07:26 +0200

    Updates

---
+++ precache-manifest.73fcf0588497c9c55506a02e4fec40ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7ce6934463c53c17a70a1d7f51bcd4",
+    "url": "/nim-game/index.html"
+  },
+  {
+    "revision": "aaad9e4e2e6f6de72a7d",
+    "url": "/nim-game/static/js/2.73968144.chunk.js"
+  },
+  {
+    "revision": "6042a33ae1766b78da66",
+    "url": "/nim-game/static/js/main.54db97a5.chunk.js"
+  },
+  {
+    "revision": "f27078f89592780df6ac",
+    "url": "/nim-game/static/js/runtime-main.f2f69a08.js"
+  }
+]);
